fbbc16e6378a80121decc6526be860a667b00f65
Andrey <9771919@mail.ru>
2019-04-08 11:41:35 +0300

    Наш второй комит

---
+++ Project_1/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+
+
+let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone.name);
+
+let arr ['apple.png', 'orange.png', apple2.png];
+
+console.log(arr[1]);
+
+alert("Hellow World");
+
+let answer = promt("Есть ли вам 18 лет?", "Да");
+console.log(answer);
---
+++ Project_1/tempCodeRunnerFile.js
@@ -0,0 +1,3 @@
+let arr ['apple.png', 'orange.png', apple2.png];
+
+console.log(arr[1]);
